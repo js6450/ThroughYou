611690aaf0f791acbe249376cf755bdf9f596446
batu <ba921@nyu.edu>
2016-02-06 14:18:25 +0400

    working REQUEST!

--- mailer.php
+++ mailer.php
@@ -3,37 +3,49 @@
     // Only process POST reqeusts.
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
         // Get the form fields and remove whitespace.
-        $name = strip_tags(trim($_POST["name"]));
-				$name = str_replace(array("\r","\n"),array(" "," "),$name);
-        $email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
-        $message = trim($_POST["message"]);
+        $sender = strip_tags(trim($_POST["sender"]));
+        $sender = str_replace(array("\r","\n"),array(" "," "),$sender);
+        $receiver = strip_tags(trim($_POST["receiver"]));
+        $address = $_POST['addr'];
+        $writer = $_POST['writer'];
+        $emailList = array('alyssa.fromstranger@gmail.com', 'angela.fromstranger@gmail.com', 'batu.fromstranger@gmail.com',
+            'faizan.fromstranger@gmail.com','grega.fromstranger@gmail.com','jiwon.fromstranger@gmail.com',
+            'leo.fromstranger@gmail.com','shoya.fromstranter@gmail.com','paula.fromstranger@gmail.com',
+            'sofia.fromstranger@gmail.com','willem.fromstranger@gmail.com');
+        
+        $writerEmail = $emailList[$writer];
+        $language = $_POST["lang"];
+        $message = trim($_POST["addinfo"]);
         
 
         // Check that data was sent to the mailer.
-        if ( empty($name) OR empty($message) OR !filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        if ( empty($sender) OR empty($message) ) {
             // Set a 400 (bad request) response code and exit.
             http_response_code(400);
             echo "Oops! There was a problem with your submission. Please complete the form and try again.";
             exit;
         }
 
-        // Set the recipient email address.
+        // Set the writerEmail email address.
         // FIXME: Update this to your desired email address.
-        $recipient = "batuaytemiz@gmail.com";
+        
+        //$writerEmail = "batuaytemiz@gmail.com";
 
         // Set the email subject.
-        $subject = "New contact from $name";
+        $subject = "From Stranger With Love Message Request";
 
         // Build the email content.
-        $email_content = "Name: $name\n";
-        $email_content .= "Email: $email\n\n";
+        $email_content = "Senders Name: $sender\n";
+        $email_content .= "Recepients Name: $receiver\n";
+        $email_content .= "Language: $language\n";
+        $email_content .= "Address: $address\n\n";
         $email_content .= "Message:\n$message\n";
 
         // Build the email headers.
-        $email_headers = "From: $name <$email>";
+        $email_headers = "From Stranger With Love";
 
         // Send the email.
-        if (mail($recipient, $subject, $email_content, $email_headers)) {
+        if (mail($writerEmail, $subject, $email_content, $email_headers)) {
             // Set a 200 (okay) response code.
             http_response_code(200);
             echo "Thank You! Your message has been sent.";
